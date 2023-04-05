f7b012dd2a417c74ac5e9ea1d8e36c786ed68bfe
Marc van Kampen <94123492+MarcvKampen@users.noreply.github.com>
2023-04-05 10:56:55 +0200

    Update 2_add_study_group.py

--- combine_prev_steps/2_add_study_group.py
+++ combine_prev_steps/2_add_study_group.py
@@ -6,16 +6,16 @@ asymptomatic = ['UWA626', 'UWA596', 'UWA419', 'UWA422', 'UWA556', 'UWA558']
 control = ['UWA479', 'UWA420', 'UWA506', 'UWA531', 'UWA431', 'UWA623']
 symptomatic = ['UWA576', 'UWA579', 'UWA580', 'UWA612', 'UWA614', 'UWA530']
 
-# define the argument parser
-parser = argparse.ArgumentParser(description='Update PSM_ID column in a CSV file.')
-parser.add_argument('input_file', type=str, help='path to the input CSV file')
-parser.add_argument('output_file', type=str, help='path to the output CSV file')
+# define the input arguments
+parser = argparse.ArgumentParser(description='rewrite PSM_ID column to study group')
+parser.add_argument('input_file', type=str, help='path to .csv file')
+parser.add_argument('output_file', type=str, help='path to .csv file')
 args = parser.parse_args()
 
-# read the CSV file and write the updated rows to a new file
+# read the .csv file and write the updated rows to a new file
 with open(args.input_file, newline='') as infile, open(args.output_file, 'w', newline='') as outfile:
     reader = csv.DictReader(infile)
-    fieldnames = reader.fieldnames
+    fieldnames = ['study_group'] + [f for f in reader.fieldnames if f != 'PSM_ID']
     writer = csv.DictWriter(outfile, fieldnames=fieldnames)
     writer.writeheader()
     # iterate through each row
@@ -24,10 +24,10 @@ with open(args.input_file, newline='') as infile, open(args.output_file, 'w', ne
         uwa_id = row['PSM_ID'].split('.')[0].split('_')[0]
         # check if the UWAxxx matches any of the parameters
         if uwa_id in asymptomatic:
-            row['PSM_ID'] = 'Asymptomatic AD'
+            row['study_group'] = 'Asymptomatic AD'
         elif uwa_id in control:
-            row['PSM_ID'] = 'Control'
+            row['study_group'] = 'Control'
         elif uwa_id in symptomatic:
-            row['PSM_ID'] = 'Symptomatic AD'
+            row['study_group'] = 'Symptomatic AD'
         # write the updated row to the output file
-        writer.writerow({k: row[k] if k != 'PSM_ID' else row['PSM_ID'].split('.')[0] for k in fieldnames})
+        writer.writerow({k: row[k] for k in fieldnames})
